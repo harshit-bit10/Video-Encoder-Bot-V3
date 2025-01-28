6f447f00b49416f3492889f9e85847c12b5c9b65
harshit-bit10 <109902163+harshit-bit10@users.noreply.github.com>
2025-01-28 23:07:47 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -6,28 +6,28 @@ from bot.get_cfg import get_config
 
 class Config(object):
     # You can keep this default
-    SESSION_NAME = get_config("SESSION_NAME", "AHCompressorBot")
+    SESSION_NAME = get_config("SESSION_NAME", "STICompressorBot")
     # AHCompressBot....
     # sucks Dude
-    APP_ID = int(get_config("APP_ID", 12345))
-    API_HASH = get_config("API_HASH", "")
-    LOG_CHANNEL = get_config("LOG_CHANNEL")
-    UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", None) # Without `@` LOL
+    APP_ID = int(get_config("APP_ID", 7603458))
+    API_HASH = get_config("API_HASH", "910e420f1f74f40305a684a331dade35")
+    LOG_CHANNEL = get_config("-1002368843413")
+    UPDATES_CHANNEL = get_config("UPDATES_CHANNEL", SharkToonsIndia) # Without `@` LOL
      # Get these values from my.telegram.org
     AUTH_USERS = set(
         int(x) for x in get_config(
-            "AUTH_USERS",
+            "6066102279",
             should_prompt=True
         ).split()
     )
 # array , simplest method was AUTH_USERS = [] ; AUTH_USERS.append(your telegram id) 🤣
     # array to store the channel ID who are authorized to use the bot
     # dont u fucking remove this id 😤
-    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = get_config("TG_BOT_TOKEN", "7267043935:AAEVAwrnYA6EFvCwxfFowfCwZM96ooUjMYU")
     # the download location, where the HTTP Server runs
     DOWNLOAD_LOCATION = get_config("DOWNLOAD_LOCATION", "/app/downloads")
     # Telegram maximum file upload size
-    BOT_USERNAME = get_config("BOT_USERNAME", "")
+    BOT_USERNAME = get_config("BOT_USERNAME", "@sharktplaybot")
     MAX_FILE_SIZE = 2097152000
     TG_MAX_FILE_SIZE = 2097152000
     FREE_USER_MAX_FILE_SIZE = 2097152000
